b69de727e4206cade8e888beb5e1c76e93bca34a
baoxiny666 <36787006+baoxiny666@users.noreply.github.com>
2020-07-14 10:45:17 +0800

    修改了第一个 github文件
    修改了第一个 github文件

--- bxyfirst001.java
+++ bxyfirst001.java
@@ -1,6 +1,6 @@
 pubilc class HelloWorld(){
   pubklic static void main(String args[]){
-      System.out.println("666666");
+      System.out.println("666666");  
      System.out.println("888888");
   }
 }
